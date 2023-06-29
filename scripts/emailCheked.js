4d87b493f4c3af4ba275b2a8f43003068c893e83
Rekonir <zhed-nikita@mail.ru>
2023-06-29 12:08:07 +0300

    fix check img of email

--- scripts/emailCheked.js
+++ scripts/emailCheked.js
@@ -1,4 +1,6 @@
 const emailInp = document.getElementById('E-mail')
+const checkImg = document.getElementsByClassName('email__check')[0]
+console.dir(checkImg)
 emailInp.addEventListener('input', function () {
     const emailValue = emailInp.value;
     const isValidEmail = emailValue.includes('@') && emailValue.includes('.');
@@ -6,9 +8,12 @@ emailInp.addEventListener('input', function () {
     if (!isValidEmail) {
         emailInp.style.borderBottom = '0.7px solid #FF2828'
         emailInp.style.color = '#FF2828';
+        checkImg.style.display = 'none'
+
     } else {
         emailInp.style.border = 'none';
         emailInp.style.color = 'black';
+        checkImg.style.display = 'block'
 
     }
 });
